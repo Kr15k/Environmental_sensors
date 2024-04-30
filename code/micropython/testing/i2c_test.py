7da43c24c6ec59f2cda393f7b730e3bdab828027
Kr15k <144447496+Kr15k@users.noreply.github.com>
2024-04-30 14:12:38 +0200

    Moved code

--- code/micropython/testing/i2c_test.py
+++
@@ -1,7 +0,0 @@
-# check what i2c addresses are to be found on the chosen sda, scl
-import machine
-i2c = machine.I2C(0,
-                  scl=machine.Pin(17),
-                  sda=machine.Pin(16),
-                  freq=400000)
-print(i2c.scan())
